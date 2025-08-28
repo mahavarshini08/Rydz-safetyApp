4e4175d158a3757201029c5ec2c99024da7f4af0
Madhu <madhumitha9760@gmail.com>
2025-08-28 21:50:40 +0530

    package.json separated

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,4 @@
+
 // frontend/config.js
-export const API_URL = "http://192.168.1.7:4000";
+export const API_URL = 'http://192.168.1.7:4000';     // backend REST server
+export const SOCKET_URL = 'http://192.168.1.7:4001';  // socket server if you have one
